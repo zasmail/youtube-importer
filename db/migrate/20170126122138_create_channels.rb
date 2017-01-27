00f044b6c2bc801e94521c24d4a5d3323a42cb01
zasmail <zasmail@utexas.edu>
2017-01-27 20:12:47 +0100

    Create channel table

--- db/migrate/20170126122138_create_channels.rb
+++ db/migrate/20170126122138_create_channels.rb
@@ -1,6 +1,6 @@
 class CreateChannels < ActiveRecord::Migration[5.0]
   def change
-    change_table :channels do |t|
+    create_table :channels do |t|
       t.string :name
       t.string :url
       t.datetime :last_updated
