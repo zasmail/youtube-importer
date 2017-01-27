2eaaae285c6404c29a6cf1fad745e703f1b29506
zasmail <zasmail@utexas.edu>
2017-01-27 21:44:36 +0100

    Switch order of jobs

--- app/controllers/get_data_controller.rb
+++ app/controllers/get_data_controller.rb
@@ -1,8 +1,8 @@
 class GetDataController < ApplicationController
   def index
     Channel.all.each do |channel|
-      ImportTalksFromChannelJob.perform_later(channel)
       ImportPlaylistsFromChannelJob.perform_later(channel)
+      ImportTalksFromChannelJob.perform_later(channel)
       UpdateSocialMediaNumbersJob.set(wait: 5.hours).perform_later()
     end
     redirect_to admin_dashboard_path
