227a782a962acf4e10098bb8dd4ddfd9ee2510f7
zasmail <zasmail@utexas.edu>
2017-01-28 01:19:09 +0100

    run social

--- app/controllers/get_data_controller.rb
+++ app/controllers/get_data_controller.rb
@@ -1,9 +1,9 @@
 class GetDataController < ApplicationController
   def index
     Channel.all.each do |channel|
-      ImportPlaylistsFromChannelJob.perform_later(channel)
-      ImportTalksFromChannelJob.perform_later(channel)
-      UpdateSocialMediaNumbersJob.set(wait: 2.hours).perform_later()
+      # ImportPlaylistsFromChannelJob.perform_later(channel)
+      # ImportTalksFromChannelJob.perform_later(channel)
+      UpdateSocialMediaNumbersJob.perform_later
     end
     redirect_to admin_dashboard_path
   end
